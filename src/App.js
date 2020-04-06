1b49a9eda887c83ee0dc3dcad40e702d0fa4ace6
slim001992 <slim1992@live.fr>
2020-04-06 11:59:04 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import MovieApp from "./components/MovieApp.js"
+import './App.css';
+function App () {
+    return (
+      <MovieApp  />
+    )
+}
+
+export default App;
